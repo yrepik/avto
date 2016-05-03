c86eaedffc7f2bb81b6d8ffb6427a5e7c486e319
gorotto <yastreb92@inbox.ru>
2016-05-03 17:17:37 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,12 @@ var gulp = require('gulp'), // Подключаем Gulp
     autoprefixer = require('gulp-autoprefixer'); // Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function() { // Создаем таск Sass
-    return gulp.src('app/sass/**/*.sass') // Берем источник
+    return gulp.src('app/sass/**/*.scss') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
             cascade: true
         })) // Создаем префиксы
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({
             stream: true
         })) // Обновляем CSS на странице при изменении
@@ -26,7 +26,7 @@ gulp.task('sass', function() { // Создаем таск Sass
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browserSync
         proxy: {
-            target: 'wp.dev' // Директория для сервера - app
+            target: 'wp-proj' // Директория для сервера - app
         },
         ghostMode: {
             clicks: true,
@@ -57,7 +57,7 @@ gulp.task('css-libs', ['sass'], function() {
 });
 
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
-    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/sass/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
     gulp.watch('./**/*.php', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 });
--- header.php
+++ header.php
@@ -27,32 +27,5 @@
 </head>
 <body <?php body_class(); // все классы для body ?>>
 	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<nav class="navbar navbar-default">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topnav" aria-expanded="false">
-								<span class="sr-only">Меню</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
-						<div class="collapse navbar-collapse" id="topnav">
-							<?php $args = array( // опции для вывода верхнего меню, чтобы они работали, меню должно быть создано в админке
-								'theme_location' => 'top', // идентификатор меню, определен в register_nav_menus() в functions.php
-								'container'=> false, // обертка списка, тут не нужна
-						  		'menu_id' => 'top-nav-ul', // id для ul
-						  		'items_wrap' => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>',
-								'menu_class' => 'top-menu', // класс для ul, первые 2 обязательны
-						  		'walker' => new bootstrap_menu(true) // верхнее меню выводится по разметке бутсрапа, см класс в functions.php, если по наведению субменю не раскрывать то передайте false		  		
-					  			);
-								wp_nav_menu($args); // выводим верхнее меню
-							?>
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
+	
 	</header>
