8140e6da70f6cca3ae16282a51dba88af8fd9083
gorotto <yastreb92@inbox.ru>
2016-05-03 14:29:51 +0300

    first

---
+++ functions.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Функции шаблона (function.php)
+ * @package WordPress
+ * @subpackage your-clean-template-3
+ */
+
+define('TM_DIR', get_template_directory(__FILE__));
+define('TM_URL', get_template_directory_uri(__FILE__));
+
+require_once TM_DIR . '/lib/Parser.php';
+require_once TM_DIR . '/lib/bootstrap_menu.php';
+require_once TM_DIR . '/lib/clean_comments_constructor.php';
+
+function typical_title() { // функция вывода тайтла
+	global $page, $paged; // переменные пагинации должны быть глобыльными
+	wp_title('', true, 'right'); // вывод стандартного заголовка без разделителя
+	$site_description = get_bloginfo('description', 'display'); // получаем описание сайта
+	if ($site_description && (is_home() || is_front_page())) //если описание сайта есть и мы на главной
+		echo " | $site_description"; // выводим описание сайта с "|" разделителем
+	if ($paged >= 2 || $page >= 2) // если пагинация была использована
+		echo ' | '.sprintf(__( 'Страница %s'), max($paged, $page)); // покажем номер страницы с "|" разделителем
+}
+
+register_nav_menus(array( // Регистрируем 2 меню
+	'top' => 'Верхнее', // Верхнее
+	'bottom' => 'Внизу' // Внизу
+));
+
+add_theme_support('post-thumbnails'); // включаем поддержку миниатюр
+set_post_thumbnail_size(250, 150); // задаем размер миниатюрам 250x150
+add_image_size('big-thumb', 400, 400, true); // добавляем еще один размер картинкам 400x400 с обрезкой
+
+register_sidebar(array( // регистрируем левую колонку, этот кусок можно повторять для добавления новых областей для виджитов
+	'name' => 'Сайдбар', // Название в админке
+	'id' => "sidebar", // идентификатор для вызова в шаблонах
+	'description' => 'Обычная колонка в сайдбаре', // Описалово в админке
+	'before_widget' => '<div id="%1$s" class="widget %2$s">', // разметка до вывода каждого виджета
+	'after_widget' => "</div>\n", // разметка после вывода каждого виджета
+	'before_title' => '<span class="widgettitle">', //  разметка до вывода заголовка виджета
+	'after_title' => "</span>\n", //  разметка после вывода заголовка виджета
+));
+
+function pagination() { // функция вывода пагинации
+	global $wp_query; // текущая выборка должна быть глобальной
+	$big = 999999999; // число для замены
+	$links = paginate_links(array( // вывод пагинации с опциями ниже
+		'base' => str_replace($big,'%#%',esc_url(get_pagenum_link($big))), // что заменяем в формате ниже
+		'format' => '?paged=%#%', // формат, %#% будет заменено
+		'current' => max(1, get_query_var('paged')), // текущая страница, 1, если $_GET['page'] не определено
+		'type' => 'array', // нам надо получить массив
+		'prev_text'    => 'Назад', // текст назад
+    	'next_text'    => 'Вперед', // текст вперед
+		'total' => $wp_query->max_num_pages, // общие кол-во страниц в пагинации
+		'show_all'     => false, // не показывать ссылки на все страницы, иначе end_size и mid_size будут проигнорированны
+		'end_size'     => 15, //  сколько страниц показать в начале и конце списка (12 ... 4 ... 89)
+		'mid_size'     => 15, // сколько страниц показать вокруг текущей страницы (... 123 5 678 ...).
+		'add_args'     => false, // массив GET параметров для добавления в ссылку страницы
+		'add_fragment' => '',	// строка для добавления в конец ссылки на страницу
+		'before_page_number' => '', // строка перед цифрой
+		'after_page_number' => '' // строка после цифры
+	));
+ 	if( is_array( $links ) ) { // если пагинация есть
+	    echo '<ul class="pagination">';
+	    foreach ( $links as $link ) {
+	    	if ( strpos( $link, 'current' ) !== false ) echo "<li class='active'>$link</li>"; // если это активная страница
+	        else echo "<li>$link</li>"; 
+	    }
+	   	echo '</ul>';
+	 }
+}
+
+add_action('wp_footer', 'add_scripts'); // приклеем ф-ю на добавление скриптов в футер
+function add_scripts() { // добавление скриптов
+    if(is_admin()) return false; // если мы в админке - ничего не делаем
+    wp_deregister_script('jquery'); // выключаем стандартный jquery
+    wp_enqueue_script('libs',get_template_directory_uri().'/js/libs.min.js','','',true); // добавляем свой
+    wp_enqueue_script('custom-scripts', get_template_directory_uri().'/js/script.min.js','','',true); // бутстрап
+    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js','','',true); // и скрипты шаблона
+	wp_localize_script('main', 'myajax',
+		array(
+			'url' => admin_url('admin-ajax.php')
+		)
+	);
+}
+
+add_action('wp_print_styles', 'add_styles'); // приклеем ф-ю на добавление стилей в хедер
+function add_styles() { // добавление стилей
+    if(is_admin()) return false; // если мы в админке - ничего не делаем
+    wp_enqueue_style( 'libs', get_template_directory_uri().'/css/libs.min.css' );// бутстрап
+    wp_enqueue_style( 'custom-styles', get_template_directory_uri().'/css/style.min.css' ); 
+	wp_enqueue_style( 'main', get_template_directory_uri().'/style.css' ); // основные стили шаблона
+}
+
+function prn($content) {
+	echo '<pre style="background: lightgray; border: 1px solid black; padding: 2px">';
+	print_r ( $content );
+	echo '</pre>';
+}
+
+function content_class_by_sidebar() { // функция для вывода класса в зависимости от существования виджетов в сайдбаре
+	if (is_active_sidebar( 'sidebar' )) { // если есть
+		echo 'col-sm-9'; // пишем класс на 80% ширины
+	} else { // если нет
+		echo 'col-sm-12'; // контент на всю ширину
+	}
+}
+
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp'), // Подключаем Gulp
+    sass = require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync = require('browser-sync'), // Подключаем Browser Sync
+    concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    cssnano = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+    del = require('del'), // Подключаем библиотеку для удаления файлов и папок
+    imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+    pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+    cache = require('gulp-cache'), // Подключаем библиотеку кеширования
+    autoprefixer = require('gulp-autoprefixer'); // Подключаем библиотеку для автоматического добавления префиксов
+
+gulp.task('sass', function() { // Создаем таск Sass
+    return gulp.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        })) // Создаем префиксы
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({
+            stream: true
+        })) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        proxy: {
+            target: 'wp.dev' // Директория для сервера - app
+        },
+        ghostMode: {
+            clicks: true,
+            forms: true,
+            scroll: true
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+            'app/libs/jquery/dist/jquery.min.js',
+            'app/libs/bootstrap/dist/js/bootstrap.min.js', // Берем jQuery
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('js')); // Выгружаем в папку app/js
+});
+
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/libs.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({
+            suffix: '.min'
+        })) // Добавляем суффикс .min
+        .pipe(gulp.dest('css')); // Выгружаем в папку app/css
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('./**/*.php', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+});
+
+/*gulp.task('clean', function() {
+return del.sync('dist'); // Удаляем папку dist перед сборкой
+});*/
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(cache(imagemin({ // Сжимаем их с наилучшими настройками с учетом кеширования
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('img')); // Выгружаем на продакшен
+});
+
+gulp.task('build', ['img', 'sass', 'scripts'], function() {
+
+    var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+            'app/css/main.css',
+            'app/css/libs.min.css'
+        ])
+        .pipe(gulp.dest('css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+        .pipe(gulp.dest('fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+        .pipe(gulp.dest('js'))
+
+});
+
+gulp.task('clear', function(callback) {
+    return cache.clearAll();
+});
+gulp.task('default', ['watch']);
