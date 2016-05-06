b285c0e54cc27352bb3d09aeb928da24c994827d
gorotto <yastreb92@inbox.ru>
2016-05-06 17:08:39 +0300

    maybe all

--- index.php
+++ index.php
@@ -51,6 +51,88 @@ get_header(); // подключаем header.php ?>
 		</div>
 	</div>
 </section>
+<section class="catalog">
+	<div class="container">
+		<h2><b>Каталог</b> нашей продукции</h2>
+		<h4>Заполните форму <b>“узнать цену”</b> прямо сейчас и наши менеджеры свяжуться с вами и
+дадут полную информацию по цене и наличию запчасти</h4>
+<div class="catalog__flex">
+	<div class="catalog__item">
+			<h4>Детали двигателя</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/engine.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>Ремни ГРМ, ролики, датчики и многое другое</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>тормозная система</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/brake.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>тормозные диски, колодки, суппорта</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>детали подвески</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/suspension.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>амортизаторы, рычаги, сайлентблоки, стойки</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>детали сцепления</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/clutch.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>диск сцепления, нажимной диск, система управления,  маховик</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>охлаждение</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/cooling.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>радиаторы, водяные насосы, термостаты</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>фильтра</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/filter.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>Масляные, воздушные, топливные, гидравлические</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>масла и жидкости</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/oil.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>моторные, трансмиссионные жидкости ГУРа, и АКПП</p>
+			</div>	
+		</div>
+		<div class="catalog__item">
+			<h4>аккумуляторы</h4>
+			<div class="catalog__item_pic">
+			<img src="<?php bloginfo('template_directory'); ?>/img/battrey.png" alt="" />
+			</div>
+			<div class="catalog__item_text">
+				<p>Для легковых и грузовыз авто и мотоциклов</p>
+			</div>	
+		</div>
+</div>
+		
+	</div>
+</section>
 <section class="car_one">
 	<div class="container">
 		<h2><b>Не нашли</b> комплектующие для своего авто?</h2>
@@ -101,19 +183,51 @@ get_header(); // подключаем header.php ?>
 			<div class="comment__item_thumb">
 				<img src="<?php bloginfo('template_directory'); ?>/img/userpic_1.png" alt="" />
 			</div>
-			<div class="comment__item_text"></div>
+			<div class="comment__item_text">
+				<h5><b>Дмитрий король</b><br>г.Донецк</h5>
+				<p>Не ожидал такой оперативности. Менеджер буквально через пять минут позвонил, и уточнил заказ.Подвеску доставили домой, даже не пришлось заморачиваться что бы ее доставить. Сработали оперативно, а главное удивили цены. Пообщали сделать на следующую покупку скидку. В общем молодцы. Я доволен. Рекомендую.
+				</p>
+			</div>
 		</div>
 		<div class="comment__item">
 			<div class="comment__item_thumb">
 				<img src="<?php bloginfo('template_directory'); ?>/img/userpic_2.png" alt="" />
 			</div>
-			<div class="comment__item_text"></div>
+			<div class="comment__item_text">
+				<h5><b>Анна Весникова </b><br>г.Донецк</h5>
+				<p>  Машину купила недавно и столкнулась с проблемой замены масла. После того. как я оставила заявку и менеджер перезвонил через 5 минут и порекомендовал приехать в сервис для замены масла. Сделали все быстро и качественно, даже объяснили как долить масло в следующий раз.буду обращаться еще!</p>
+			</div>
 		</div>
 		<div class="comment__item">
 			<div class="comment__item_thumb">
 				<img src="<?php bloginfo('template_directory'); ?>/img/userpic_3.png" alt="" />
 			</div>
-			<div class="comment__item_text"></div>
+			<div class="comment__item_text">
+				<h5><b>Кирилл Бойко</b><br>г.Донецк</h5>
+				<p>долго искал топливный насос на китайский мопед. Оставил заявку на деталь, перезвонили и сообщили, что в наличии нет, но могут через некоторое время привезти. Все заняло около 3-х дней. На цену доставка никак не повлияла. Очень доволен. Спасибо.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="footer_form">
+	<div class="section">
+		<div class="border">
+			<h3>магазин <br> новых автозапчастей для любых автомобилей</h3>
+			<form action="" class="footer_form_end" novalidate  method="post">
+				<h2><b>Заполните форму</b><br> и получите скидку в 5%</h2>
+				<div class="form_line">
+					<input type="text" name="name" placeholder="Ваше имя">
+					<input type="tel" name="tel" placeholder="Ваш телефон">
+					<input type="email" name="email" placeholder="Ваш Email">
+					<button><h4>Отправить</h4></button>
+				</div>
+				
+			</form>
+			<img src="<?php bloginfo('template_directory'); ?>/img/footer_car.png" alt="" />
+			<!--<div class="connection">
+				<button><h4>заказать звонок</h4></button>
+				<button><h4>пишите нам в skype</h4></button>
+			</div>-->
 		</div>
 	</div>
 </section>
